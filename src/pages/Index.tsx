cbd1028cb116e7fb02e676b96cf16b25f68c64ab
Yarbis Beltré🌴🥥 <yarbisbeme@gmail.com>
2025-08-25 13:04:41 -0400

    NosotrosSection update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,28 +3,24 @@ import HeroSection from "@/components/HeroSection";
 import ScheduleSection from "@/components/ScheduleSection";
 import SananosSection from "@/components/SananosSection";
 import CongressSection from "@/components/CongressSection";
-import MissionVisionSection from "@/components/MissionVisionSection";
+import MissionVisionSection from "@/components/Nosotros";
 import MissionsSection from "@/components/MissionsSection";
 import MinistriesSection from "@/components/MinistriesSection";
 import { Footer } from "@/components/Footer";
 
 const Index = () => {
-  const openWhatsApp = () => {
-    // Logic to open WhatsApp
-    console.log("Opening WhatsApp...");
-  };
 
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen ">
       <Navbar />
       <div id="hero">
         <HeroSection />
       </div>
-      <div id="schedule">
-        <ScheduleSection />
-      </div>
       <div id="mission">
         <MissionVisionSection />
+      </div>
+      <div id="schedule">
+        <ScheduleSection />
       </div>
       <div id="sananos">
         <SananosSection />
@@ -39,7 +35,7 @@ const Index = () => {
         <MinistriesSection />
       </div>
       <div>
-        <Footer openWhatsApp={openWhatsApp} />
+        <Footer />
       </div>
     </div>
   );
